bf82cc513606976c622584659962a4ec885ecf75
praharshithaamdhipuram-collab <praharshithaamdhipuram@gmail.com>
2026-09-08 20:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,9 @@ formatted_agent_chain = (
 
 # --- 3. FastAPI App ---
 ##Need To Code
-app=FASTAPI(name="india weather and cinema agent")
-add_routes(app, formatted_agent_chain, path="\agent")
+app=FastAPI(name="india weather and cinema agent")
+add_routes(app, formatted_agent_chain, path="\agent",playground_type="default")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
